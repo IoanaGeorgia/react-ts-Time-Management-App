b5a0e209cf916ac14e944afc0bae0b1cfb84bbb6
IoanaGeorgia <91886439+IoanaGeorgia@users.noreply.github.com>
2023-02-11 23:52:12 +0200

    1 file modified, 2 added

--- Body.tsx
+++ Body.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import style from './styles/Body.module.css';
 import Offers from './Offers';
-import Info from './Info'
+import Info from './Info';
+import Screen from './Screen';
 
 
 export default function Body() {
   return (
     <div>
+      <Screen />
       <Offers/>
       <Info />
     </div>
---
+++ Screen.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import style from './styles/Screen.module.css'
+
+
+export default function Screen() {
+  return (
+    <div className={style.screenBackground}>
+    </div>
+  );
+}
