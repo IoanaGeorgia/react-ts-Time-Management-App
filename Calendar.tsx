a26a2ab85fadb17e919306f9b7c6beddc3ccbbbf
IoanaGeorgia <91886439+IoanaGeorgia@users.noreply.github.com>
2023-02-12 11:38:42 +0200

    1 file modified, 2 added

---
+++ Calendar.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import style from './styles/Body.module.css'
+
+
+
+export default function Calendar() {
+  return (
+    <div className={style.offersBody}>
+
+    </div>
+  );
+}
--- Screen.tsx
+++ Screen.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import style from './styles/Screen.module.css'
+import Calendar from './Calendar'
+import Tasks from './Tasks'
 
 
 export default function Screen() {
   return (
     <div className={style.screenBackground}>
+      <Calendar />
+      <Tasks />
     </div>
   );
 }
