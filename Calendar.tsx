6772c5c6277c87e8733f5b9f5f9436b004264de5
IoanaGeorgia <91886439+IoanaGeorgia@users.noreply.github.com>
2023-04-06 14:08:29 +0300

    2 files modified

--- Calendar.tsx
+++ Calendar.tsx
@@ -8,21 +8,75 @@ export default function Calendar() {
   const [expand, setExpand] =useState(false)
   const date= new Date()
   const day = date.getDate()
+  const year = date.getFullYear()
+  const month= date.getMonth()
+  function monthDone(month){
+    let monthDone;
+    switch (month) {
+      case 0:
+        monthDone = "January";
+        break;
+      case 1:
+        monthDone = "February";
+        break;
+      case 2:
+        monthDone = "March";
+        break;
+      case 3:
+        monthDone = "April";
+        break;
+      case 4:
+        monthDone = "May";
+        break;
+      case 5:
+        monthDone = "June";
+        break;
+      case 6:
+        monthDone = "July";
+    case 7:
+      monthDone = "August";
+      break;
+    case 8:
+      monthDone = "September";
+      break;
+    case 9:
+      monthDone = "October";
+      break;
+    case 10:
+      monthDone = "November";
+        case 11:
+  monthDone = "December";
+  break;
+}
+    return monthDone
+  }
+
 
   return (
     <div className={style.calendar}>
-      <button className='buttonToggleCalendar' onClick={()=>setExpand(!expand)}>Open Calendar</button>
+      
+      <button className={style.buttonToggleCalendar} onClick={()=>setExpand(!expand)}>See more</button>
 
       {expand && (
-                <iframe src="https://calendar.google.com/calendar/embed?src=ro.new_zealand%23holiday%40group.v.calendar.google.com&ctz=Europe%2FBucharest" style={{width:'300px', height:'300px'}}></iframe>
-         )} 
-
+                <iframe src="https://calendar.google.com/calendar/embed?src=ro.new_zealand%23holiday%40group.v.calendar.google.com&ctz=Europe%2FBucharest" style={{width:'90%', height:'80%'}}></iframe>
+                )} 
+      {!expand && (
         <div className={style.today}>
 
-            <div style={{color:'pink', fontWeight:'bolder', fontSize:'20px'}}>
+            <div style={{color:'hotpink', fontWeight:'bolder', fontSize:'90px', textAlign:'center', height:'55%', display:'flex', justifyContent:'center', alignItems:'center'}}>
             {day}
             </div>
+
+
+            <div style={{color:'hotpink', fontWeight:'normal', fontSize:'20px', textAlign:'center', height:'20%'}}>
+            {monthDone(month)}
+            </div>
+
+            <div style={{color:'black', backgroundColor:'hotpink', fontWeight:'bolder', fontSize:'25px', textAlign:'center', height:'15%'}}>
+            {year}
+            </div>
           </div>
+          )} 
     </div>
   );
 }
